63cc9620c9cb56601afe8de2befc250eb1e708ab
Vincent van Hees <vincentvanhees@gmail.com>
2021-03-19 12:39:46 +0100

    filepaths are now stored in external file

--- main.R
+++ main.R
@@ -1,18 +1,15 @@
 ### Load data and select windows
 
 # user input required:
-# Set path to data folder
-# path <- "/Users/annelindelettink/Documents/Work MacBook Pro Annelinde/Mechanical Shaker Machine/mechanicalshakerexperiments/data"
-path <- "/home/vincent/data/Annelinde/shaker_experiments"
-setwd(path) #set path
-# install_github("wadpac/mechanicalshakermachineexperiments")
-#load functions
+# Obtain path to data folder
+# this is csv file with two columns: my_data_folder and my_functions_folder
+# in the second row you will store the respective folder paths as a character
+filepaths = read.csv("data/filepaths.csv")
+my_data_folder = filepaths$my_data_folder
+my_functions_folder = filepaths$my_functions_folder
 
-# my_functions_folder = "/Users/annelindelettink/Documents/Work MacBook Pro Annelinde/Mechanical Shaker Machine/mechanicalshakerexperiments/scripts"
-my_functions_folder = "/home/vincent/projects/mechanicalshakerexperiments/scripts"
-
+setwd(my_data_folder) #set path
 for (function_file in dir(my_functions_folder, full.names = T)) source(function_file) #load functions
-
 
 # TO DO:
 # How to select windows for protocol 1? For now: range between start and end time used, based on used sensors. Events turn box not yet included (function selectwindows)
@@ -39,42 +36,42 @@ if (length(which(slash_in_name == TRUE)) > 0) {
 
 ## Actigraph
 # Protocol 1
-# data_time_xyz_actigraph_pro1 <- loaddata(path = path, brand = "Actigraph", protocol = 1, session = 1)
+data_time_xyz_actigraph_pro1 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 1, session = 1)
 # Protocol 2
-data_time_xyz_actigraph_pro2_ses1 <- loaddata(path = path, brand = "Actigraph", protocol = 2, session = 1)
-data_time_xyz_actigraph_pro2_ses2 <- loaddata(path = path, brand = "Actigraph", protocol = 2, session = 2)
-data_time_xyz_actigraph_pro2_ses3 <- loaddata(path = path, brand = "Actigraph", protocol = 2, session = 3)
+data_time_xyz_actigraph_pro2_ses1 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 2, session = 1)
+data_time_xyz_actigraph_pro2_ses2 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 2, session = 2)
+data_time_xyz_actigraph_pro2_ses3 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 2, session = 3)
 # Protocol 3
-data_time_xyz_actigraph_pro3 <- loaddata(path = path, brand = "Actigraph", protocol = 3, session = 3)
+data_time_xyz_actigraph_pro3 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 3, session = 3)
 
 ## Activpal
 # Protocol 1
-data_time_xyz_activpal_pro1 <- loaddata(path = path, brand = "Activpal", protocol = 1, session = 1)
+data_time_xyz_activpal_pro1 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 1, session = 1)
 # Protocol 2
-data_time_xyz_activpal_pro2_ses1 <- loaddata(path = path, brand = "Activpal", protocol = 2, session = 1)
-data_time_xyz_activpal_pro2_ses2 <- loaddata(path = path, brand = "Activpal", protocol = 2, session = 2)
-data_time_xyz_activpal_pro2_ses3 <- loaddata(path = path, brand = "Activpal", protocol = 2, session = 3)
+data_time_xyz_activpal_pro2_ses1 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 2, session = 1)
+data_time_xyz_activpal_pro2_ses2 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 2, session = 2)
+data_time_xyz_activpal_pro2_ses3 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 2, session = 3)
 # Protocol 3
-data_time_xyz_activpal_pro3 <- loaddata(path = path, brand = "Activpal", protocol = 3, session = 3)
+data_time_xyz_activpal_pro3 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 3, session = 3)
 
 ##Acttrust
 # Protocol 1
-data_time_xyz_acttrust_pro1 <- loaddata(path = path, brand = "Acttrust", protocol = 1, session = 1)
+data_time_xyz_acttrust_pro1 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 1, session = 1)
 # Protocol 2
-data_time_xyz_acttrust_pro2_ses1 <- loaddata(path = path, brand = "Acttrust", protocol = 2, session = 1)
-data_time_xyz_acttrust_pro2_ses2 <- loaddata(path = path, brand = "Acttrust", protocol = 2, session = 2)
-data_time_xyz_acttrust_pro2_ses3 <- loaddata(path = path, brand = "Acttrust", protocol = 2, session = 3)
+data_time_xyz_acttrust_pro2_ses1 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 2, session = 1)
+data_time_xyz_acttrust_pro2_ses2 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 2, session = 2)
+data_time_xyz_acttrust_pro2_ses3 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 2, session = 3)
 # Protocol 3
-data_time_xyz_acttrust_pro3 <- loaddata(path = path, brand = "Acttrust", protocol = 3, session = 3)
+data_time_xyz_acttrust_pro3 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 3, session = 3)
 
 ## Axivity
 # Protocol 1
-data_time_xyz_axivity_pro1 <- loaddata(path = path, brand = "Axivity", protocol = 1, session = 1)
+data_time_xyz_axivity_pro1 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 1, session = 1)
 #Protocol 2
-data_time_xyz_axivity_pro2_ses1 <- loaddata(path = path, brand = "Axivity", protocol = 2, session = 1)
-data_time_xyz_axivity_pro2_ses2 <- loaddata(path = path, brand = "Axivity", protocol = 2, session = 2)
-data_time_xyz_axivity_pro2_ses3 <- loaddata(path = path, brand = "Axivity", protocol = 2, session = 3)
+data_time_xyz_axivity_pro2_ses1 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 2, session = 1)
+data_time_xyz_axivity_pro2_ses2 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 2, session = 2)
+data_time_xyz_axivity_pro2_ses3 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 2, session = 3)
 # Protocol 3
-data_time_xyz_axivity_pro3_ses1 <- loaddata(path = path, brand = "Axivity", protocol = 3, session = 1)
-data_time_xyz_axivity_pro3_ses2 <- loaddata(path = path, brand = "Axivity", protocol = 3, session = 2)
-data_time_xyz_axivity_pro3_ses3 <- loaddata(path = path, brand = "Axivity", protocol = 3, session = 3)
+data_time_xyz_axivity_pro3_ses1 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 3, session = 1)
+data_time_xyz_axivity_pro3_ses2 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 3, session = 2)
+data_time_xyz_axivity_pro3_ses3 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 3, session = 3)
