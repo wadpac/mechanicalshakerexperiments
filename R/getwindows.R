59302c0c91f60aa36c2e785ed38149e82f4ae349
Vincent van Hees <vincentvanhees@gmail.com>
2021-05-24 18:42:28 +0200

    Scripts folder renamed to R, GENEActiv fileloading improved

---
+++ R/getwindows.R
@@ -0,0 +1,91 @@
+# Read in data description and select relevant information based on protocol and session
+getwindows <- function(brand, protocol, session, path, data, protocolfile) {
+  library(gdata)
+  description <- read.xls(protocolfile, header = TRUE)
+  description <- description[which(description$protocol == protocol & description$session == session),]
+  # Calculate indices for the windows to select
+  start_time <- c()
+  end_time <- c()
+  tz = "Europe/Amsterdam"
+  if (protocol == 1) {
+    if (brand == "Actigraph" || brand == "Activpal") {
+      selection <- description[description$accelerometers_used == "activpal_actigraph",]
+    }
+    if (brand == "Axivity" || brand == "GENEActiv" || brand == "Acttrust" || brand == "Shimmer" || brand == "MOX"){
+      selection <- description[description$accelerometers_used == "axivity_geneactiv_acttrust_shimmer_mox",]
+    }
+    start <- strftime(paste0(selection$date[1], selection$start_time[1]), format = "%Y-%m-%d %H:%M:%OS2", tz = tz)
+    start_time <- start
+    end <- strftime(paste0(selection$date[nrow(selection)], selection$start_time[nrow(selection)]), format = "%Y-%m-%d %H:%M:%OS2", tz = tz)
+    end_time <- end
+  }
+  if (protocol == 2 || protocol == 3) {
+    for (r in 1:nrow(description)) {
+      if(startsWith(description$accelerometers_used[r], "all") || (brand == "Axivity" & description$accelerometers_used[r] == "axivity")){
+        start <- strftime(toString(paste(description$date[r], description$start_time[r]), sep = " "), format = "%Y-%m-%d %H:%M:%OS2", tz = tz)
+        start_time <- c(start_time, start)
+        end <- strftime(toString(paste(description$date[r], description$end_time[r]), sep = " "), format = "%Y-%m-%d %H:%M:%OS2", tz = tz)
+        end_time <- c(end_time, end)
+      }
+    }
+  }
+  selected_data_list <- list()
+  selected_data <- data.frame()
+  #Select the windows
+  cat("file ")
+  tz = "Europe/Amsterdam"
+  
+  for(pp in 1:length(data)) { # pp is file number?
+    cat(paste0(" ",pp))
+    if (brand %in% c("Axivity", "GENEActiv")) {
+      # GENEActiv and Axivity come with separate file header object
+      # which we do not need anymore, and by moving the data up we
+      # standardise the object structure:
+      if (brand == "GENEActiv") {
+        data[[pp]] = data[[pp]]$data.out
+      } else if (brand == "Axivity") {
+        data[[pp]] = data[[pp]]$data
+      }
+    }
+    if (nrow(data[[pp]]) > 0) {
+      selected_data = data[[pp]]
+      selected_data$shaking_freqency = -1 # default is -1 frequency
+      selected_data$condition = ""
+      for(w in 1:length(start_time)) { # w is condition within the experiment (e.g. shaker frequency)
+        if(brand == "Actigraph") {
+          stime = as.POSIXct(start_time[w], tz = tz)
+          etime = as.POSIXct(end_time[w], tz = tz)
+        } else if (brand == "Activpal") {
+          stime = as.POSIXlt(start_time[w], tz = tz)
+          etime = as.POSIXlt(end_time[w], tz = tz)
+        } else if (brand == "Acttrust") {
+          stime = as.POSIXlt(start_time[w], tz = tz)
+          etime = as.POSIXct(end_time[w], tz = tz)
+        } else if(brand == "Axivity") {
+          stime = as.POSIXct(start_time[w], tz = tz)
+          etime = as.POSIXct(end_time[w], tz = tz)
+        } else if(brand == "GENEActiv") {
+          stime = as.POSIXct(start_time[w], tz = tz)
+          etime = as.POSIXct(end_time[w], tz = tz)
+        }
+        segment = which(selected_data$time >= stime & selected_data$time < etime)
+        if(length(segment) > 0) {
+          selected_data$shaking_freqency[segment] = as.numeric(description$mechanical_shaker_setting[w])
+          selected_data$condition[segment] = as.character(description$condition[w])
+        }
+      }
+      MissingFreqs = which(selected_data$shaking_freqency == -1)
+      if (length(MissingFreqs) > 0) {
+        selected_data = selected_data[-MissingFreqs,]
+      }
+      selected_data_list[[pp]] <- selected_data
+    } else {
+      selected_data_list[[pp]] <- NULL
+    }
+  }
+  rm(selected_data)
+  return(selected_data_list)
+  
+}
+
+
---
+++ R/loaddata.R
@@ -0,0 +1,167 @@
+## Function to read in the mechanical shaker data using:
+# path;
+# brand: one of c("Actigraph", "Activpal", "Acttrust", "Axivity", "GENEActiv", "MOX", "Shimmer", "Fitbit), 
+# protocol number: 1, 2 or 3;
+# session number: 1, 2 or 3;
+# windows: TRUE then windows from the data_description file will be selected, FALSE: complete data file will be loaded.
+
+loaddata <- function(path, brand, protocol, session, windows = TRUE, protocolfile) {
+  # Load required libraries
+  library(doParallel)
+  library(GGIR)
+  library(GENEAread)
+  library(read.gt3x)
+  cat(paste0("\nAttempting to load ",brand, " | protocol: ", protocol, " | session: ", session, ":\n"))
+  #------------------------------------------------------------
+  # Get folder name of the data files and set path to files
+  if(brand == "MOX") { # temporarily change name of brand to enable getting the folder path right
+    brand <- "MOX_exportedCSV/exportedCSV/MOX" 
+  }
+  if((protocol == 1 || protocol == 3) && (brand != "Acttrust" || brand != "Fitbit")) {
+    folder <- paste0(brand, paste0("_pro", protocol))
+    if (brand == "Axivity" | brand == "GENEActiv") {
+      if (session != 1) {
+        folder <- paste0(brand, paste0(paste0("_pro", protocol), paste0("_ses", session))) 
+      } else {
+        folder <- paste0(brand, paste0(paste0("_pro", protocol))) 
+      }
+    }
+  }
+  if(protocol == 2 && (brand != "Acttrust" || brand != "Fitbit")) {
+    if (brand == "Activpal") {
+      folder <- paste0(brand, paste0("_pro", protocol)) 
+    } else {
+      folder <- paste0(brand, paste0(paste0("_pro", protocol), paste0("_ses", session))) 
+    }
+  }
+  if (brand == "Acttrust") {
+    folder <- "Acttrust_Condor"
+  } else if (brand == "Fitbit") {
+    folder <- brand
+  } else if (brand == "MOX_exportedCSV/exportedCSV/MOX") {
+    brand <- "MOX"
+  }
+  #------------------------------------------------------------
+  # Get brand specific file extensions
+  if (brand == "Actigraph") {
+    pattern ="*.gt3x"
+  } else if (brand == "Activpal" | brand == "MOX" | brand == "Shimmer") {
+    pattern ="*.csv"
+  } else if (brand == "Acttrust") {
+    pattern ="*.txt"
+  } else if (brand == "Axivity") {
+    pattern ="*.cwa"
+  } else if (brand == "GENEActiv") {
+    pattern ="*.bin"
+  }
+  #else {} #fitbit
+  #------------------------------------------------------------
+  # Get file list
+  file_path <- paste(path, folder, sep = "/")
+  if (length(file_path) == 0) {
+    warning("\nNo recordings identified. Check folder path.")
+  }
+  file_list <- list.files(file_path, pattern = pattern, all.files = FALSE)
+  print(paste0(length(file_list), " files identified for loading:"))
+  
+  # Load data in parallel
+  closeAllConnections() # in case there is a still something running from last time, kill it.
+  cores = parallel::detectCores()
+  Ncores = cores[1]
+  cl <- parallel::makeCluster(Ncores-2) #not to overload your computer
+  doParallel::registerDoParallel(cl)
+  parallelLoad <- function(file_path, file_list, brand, protocol, windows, session, path) {
+    foreach(i = 1:length(file_list), .packages = c("read.gt3x", "GGIR", "GENEAread"), .export = "read.activpal") %dopar% {
+      # for (i in 1:length(file_list)) {
+      # Load in the data
+      tz = "Europe/Amsterdam"
+      if(brand == "Actigraph") {
+        rawdata <- as.data.frame(read.gt3x(paste(file_path, file_list[i], sep = "/"), asDataFrame = TRUE))
+        rawdata$time = as.POSIXlt(rawdata$time, tz = tz)
+      } else if (brand == "Activpal") {
+        rawdata <- read.activpal(paste(file_path, file_list[i], sep = "/"))
+      } else if(brand == "Acttrust") {
+        rawdata <- read.csv(paste(file_path, file_list[i], sep = "/"), sep = ";", skip = 25)
+      } else if(brand == "Axivity") {
+        rawdata <- g.cwaread(paste(file_path, file_list[i], sep = "/"), start = 1, end = 1000000, desiredtz = tz)
+        # rawdata = rawdata$data # ignore header already at this stage
+      } else if (brand == "GENEActiv") {
+        rawdata <- read.bin(paste(file_path, file_list[i], sep = "/")) 
+        rawdata$data.out = as.data.frame(rawdata$data.out)
+        colnames(rawdata$data.out)[1] = "time"
+        rawdata$data.out$time = as.POSIXlt(rawdata$data.out$time, desiredtz = tz, origin = "1970-01-01")
+      } else if (brand == "MOX") {
+        rawdata <- read.csv(paste(file_path, file_list[i], sep = "/")) 
+      } else if (brand == "Shimmer") {
+        rawdata <- read.csv(paste(file_path, file_list[i], sep = "/"), nrow = 10, skip = 1, sep = '\t')
+      } else {
+      }# brand = Fitbit
+      
+      # make selection of the data with 2 minutes before and after each experiment
+      # this to reduce amount data in memory
+      if (protocol == 1 & session == 1) {
+        start = as.POSIXlt("2020-11-26 9:12:00", tz = tz)
+        end = as.POSIXlt("2020-11-26 19:30:00", tz = tz)
+      }
+      if (protocol == 2 & session == 1) {
+        start = as.POSIXlt("2020-11-24 9:42:00", tz = tz)
+        end = as.POSIXlt("2020-11-24 10:47:00", tz = tz)
+      }
+      if (protocol == 2 & session == 2) {
+        start = as.POSIXlt("2020-11-24 12:37:00", tz = tz)
+        end = as.POSIXlt("2020-11-24 13:20:00", tz = tz)
+      }
+      if (protocol == 2 & session == 3) {
+        start = as.POSIXlt("2020-11-24 15:07:00", tz = tz)
+        end = as.POSIXlt("2020-11-24 15:50:00", tz = tz)
+      }
+      if (protocol == 2 & session == 4) {
+        start = as.POSIXlt("2020-11-24 15:54:00", tz = tz)
+        end = as.POSIXlt("2020-11-24 16:55:00", tz = tz)
+      }
+      if (protocol == 3 & session == 1) {
+        start = as.POSIXlt("2020-11-27 8:37:00", tz = tz)
+        end = as.POSIXlt("2020-11-27 9:20:00", tz = tz)
+      }
+      if (protocol == 3 & session == 2) {
+        start = as.POSIXlt("2020-11-27 10:02:00", tz = tz)
+        end = as.POSIXlt("2020-11-27 10:45:00", tz = tz)
+      }
+      if (protocol == 3 & session == 3) {
+        start = as.POSIXlt("2020-11-27 11:32:00", tz = tz)
+        end = as.POSIXlt("2020-11-27 12:15:00", tz = tz)
+      }
+      if (protocol == 3 & session == 4) {
+        start = as.POSIXlt("2020-11-27 13:35:00", tz = tz)
+        end = as.POSIXlt("2020-11-27 13:44:00", tz = tz)
+      }
+      if (brand %in% c("Axivity")) {
+        start = as.numeric(start)
+        end = as.numeric(end)
+      }
+      if (brand %in% c("Axivity", "GENEActiv") == FALSE) {
+        rawdata = rawdata[which(rawdata$time >= start & rawdata$time <= end),] 
+      } else {
+        if (brand == "GENEActiv") {
+          rawdata$data.out = rawdata$data.out[which(rawdata$data.out$time >= start & rawdata$data.out$time <= end),]
+        } else if (brand == "Axivity") {
+          rawdata$data = rawdata$data[which(rawdata$data$time >= start & rawdata$data$time <= end),]
+        } 
+      }
+      return(rawdata)  }
+  }
+  # } # for loop
+  
+  data <- tryCatch(parallelLoad(file_path, file_list, brand, protocol, windows, session, path), error = function(e) print(e))
+  parallel::stopCluster(cl)
+  #Get data specifications
+  names(data) <- file_list
+  specifications <- getspecs(brand, data)
+  
+  
+  #Get and select windows
+  if (windows == TRUE) {
+    data <- getwindows(brand, protocol, session, path, data, protocolfile)
+  }
+  return(list(data = data, specifications = specifications))
+}
