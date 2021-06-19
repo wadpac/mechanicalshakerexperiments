9985f5488beca9b810ea2ec2fb282e474af3b142
Vincent van Hees <vincentvanhees@gmail.com>
2021-06-19 18:01:24 +0200

    loading of data moved to loop to avoid code repetition and TO DO list moved to github issue

--- main.R
+++ main.R
@@ -2,22 +2,24 @@
 rm(list=ls()) # freeing up memory
 
 # user input required:
-# specify path to data folder
 my_data_folder = "/home/vincent/data/VUMC/shaker_experiments" 
 protocolfile = paste0(my_data_folder, "/data_description_V3.xlsx")
+outputdir = "~/data/VUMC/shaker_experiments/extracteddata"
 
 # Following lines only needed when running debugging code:
 my_functions_folder =   "/home/vincent/projects/mechanicalshakerexperiments/R" 
+
+
+#=========================================================================================
 for (function_file in dir(my_functions_folder, full.names = T)) source(function_file) #load functions
 
-# TO DO:
-# How to select windows for protocol 1? For now: range between start and end time used, based on used sensors. Events turn box not yet included (function selectwindows)
-# Axivity: protocol3_ses2 data load failed
-# Acttrust: which variables for acceleration to select?
-# How to select the data for protocol session 4 (subset of devices)
+# installing GGIR separately for now to aid in experimenting with GGIR implementation
+library("remotes")
+remotes::install_github("wadpac/GGIR", ref= "resampleupdate")
+library("GGIR")
 
 # Check for missing packages and install if missing:
-packages = c("GGIR", "gdata", "doParallel", "remotes", "GENEAread", "THLfi/read.gt3x", "data.table")
+packages = c("gdata", "doParallel", "remotes", "GENEAread", "THLfi/read.gt3x", "data.table") #"GGIR",
 slash_in_name = sapply(packages, FUN = function(x) length(unlist(strsplit(x, "/"))) == 2)
 if (length(which(slash_in_name == FALSE)) > 0) {
   CRANpackages_to_get = packages[which(packages[!slash_in_name] %in% rownames(installed.packages()) == FALSE)]
@@ -34,48 +36,35 @@ if (length(which(slash_in_name == TRUE)) > 0) {
 }
 
 checkdimensions = function(x) {
-  for (j in 1:length(x)) {
-    print(dim(x[[j]]))
+  if (length(x$data) > 0) {
+    print(dim(x$data[[1]]))
   }
 }
 
-focus_pro1 = TRUE # to avoid loading all data at once as that will never be needed
+brands_to_extract = c("Actigraph", "Axivity", "GENEActiv") #"Activpal", "Acttrust", 
+focus_pro1 = FALSE # to avoid loading all data at once as that will never be needed
 if (focus_pro1 == TRUE) {
-  actigraph_pro1 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 1, session = 1, protocolfile=protocolfile)
-  activpal_pro1 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 1, session = 1, protocolfile=protocolfile)
-  acttrust_pro1 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 1, session = 1, protocolfile=protocolfile)
-  axivity_pro1 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 1, session = 1, protocolfile=protocolfile)
-  geneactiv_pro1 <- loaddata(path = my_data_folder, brand = "GENEActiv", protocol = 1, session = 1, protocolfile=protocolfile)
-  
-  print("Check dimensions of Axivity data: Protocol 1")
-  checkdimensions(axivity_pro1$data)
-  print("Check dimensions of GENEActiv data: Protocol 1")
-  print(checkdimensions(geneactiv_pro1$data))
+  for (brand in brands_to_extract) {
+    protocol = 1
+    session = 1
+    extractedata <- loaddata(path = my_data_folder, brand = brand, protocol = protocol, session = session, protocolfile=protocolfile)
+    cat(paste0("\nCheck dimensions of ", brand, ": Protocol ",protocol,"\n"))
+    checkdimensions(extractedata)
+    save_data(extractedata,outputdir=outputdir, objectname=paste0("_", brand, "_pro",protocol,"_ses", session))
+    rm(extractedata)
+  }
 } else {
-  actigraph_pro2_ses1 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 2, session = 1, protocolfile=protocolfile)
-  actigraph_pro2_ses2 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 2, session = 2, protocolfile=protocolfile)
-  actigraph_pro2_ses3 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 2, session = 3, protocolfile=protocolfile)
-  actigraph_pro3 <- loaddata(path = my_data_folder, brand = "Actigraph", protocol = 3, session = 3)
-  
-  activpal_pro2_ses1 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 2, session = 1, protocolfile=protocolfile)
-  activpal_pro2_ses2 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 2, session = 2, protocolfile=protocolfile)
-  activpal_pro2_ses3 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 2, session = 3, protocolfile=protocolfile)
-  activpal_pro3 <- loaddata(path = my_data_folder, brand = "Activpal", protocol = 3, session = 3, protocolfile=protocolfile)
-  print("Check dimensions of Activpal data: Protocol 2")
-  checkdimensions(activpal_pro2_ses1$data)
-  checkdimensions(activpal_pro2_ses2$data)
-  checkdimensions(activpal_pro2_ses3$data)
-  checkdimensions(activpal_pro3$data)
-  
-  acttrust_pro2_ses1 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 2, session = 1, protocolfile=protocolfile)
-  acttrust_pro2_ses2 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 2, session = 2, protocolfile=protocolfile)
-  acttrust_pro2_ses3 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 2, session = 3, protocolfile=protocolfile)
-  acttrust_pro3 <- loaddata(path = my_data_folder, brand = "Acttrust", protocol = 3, session = 3, protocolfile=protocolfile)
-  
-  axivity_pro2_ses1 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 2, session = 1, protocolfile=protocolfile)
-  axivity_pro2_ses2 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 2, session = 2, protocolfile=protocolfile)
-  axivity_pro2_ses3 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 2, session = 3, protocolfile=protocolfile)
-  axivity_pro3_ses1 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 3, session = 1, protocolfile=protocolfile)
-  axivity_pro3_ses2 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 3, session = 2, protocolfile=protocolfile)
-  axivity_pro3_ses3 <- loaddata(path = my_data_folder, brand = "Axivity", protocol = 3, session = 3, protocolfile=protocolfile)
+  for (brand in brands_to_extract) {
+    for (protocol in 2:3) {
+      for (session in 1:3) {
+        if (protocol == 2 | (protocol == 3 & session == 3) | (protocol == 3 & brand == "Axivity")) {
+          extractedata <- loaddata(path = my_data_folder, brand = brand, protocol = protocol, session = session, protocolfile=protocolfile)
+          cat(paste0("\nCheck dimensions of ", brand, ": Protocol ",protocol," session", session,"\n"))
+          checkdimensions(extractedata)
+          save(extractedata, file = paste0(outputdir, "/", brand, "_pro",protocol,"_ses", session))
+          rm(extractedata)
+        }
+      }
+    }
+  }
 }
