cf5c35a465bdf42c13e371f81bc4cfd65b938d8e
Vincent van Hees <vincentvanhees@gmail.com>
2021-08-06 20:28:26 +0200

    data collapsed per frequency and anova added

--- explore_data.R
+++ explore_data.R
@@ -24,7 +24,7 @@ library("MIMSunit")
 fns = dir(extracted_data_path, full.names = TRUE)
 outputfile = "~/data/VUMC/shaker_experiments/explore_MIMS.RData"
 sessionames = c("pro2_ses1", "pro2_ses2", "pro2_ses3") #"pro3_ses3" #<= ignore protocol session 3 for now as this did not have flat orientation
-overwrite= TRUE
+overwrite= FALSE
 epochsize = 5
 averageperws3 = function(x,sf,epochsize) {
   x2 =cumsum(c(0,x))
@@ -58,9 +58,6 @@ if (!file.exists(outputfile) | overwrite == TRUE) {
           DR = as.numeric(extractedata$specifications[i, "dynamic_range"])
           sf = as.numeric(extractedata$specifications[i, "sampling_frequency"])
           sn = as.character(extractedata$specifications[i, "serial_number"])
-          
-          # table(diff(tmp$time))
-          kkkkk
           if (brand == "Actigraph" | brand == "Activpal") {
             tmp$time = as.POSIXct(tmp$time, origin = "1970-01-01", tz="Europe/Amsterdam")
             tmp = tmp[, c("time","X","Y","Z","shaking_frequency")] #,"shaking_frequency"
@@ -119,8 +116,6 @@ if (!file.exists(outputfile) | overwrite == TRUE) {
           tmp$X = tmp$X / VM
           tmp$Y = tmp$Y / VM
           tmp$Z = tmp$Z / VM  #(tmp$Z - MZ) + round(MZ)
-          
-         
           # #=================
           # # Try apply code from:
           # # https://martakarass.github.io/post/2021-06-29-pa_measures_and_summarizedactigraphy/#dataset-labeled-raw-accelerometry-data
@@ -138,7 +133,7 @@ if (!file.exists(outputfile) | overwrite == TRUE) {
           x0 = Sys.time()
           S = MIMSunit::mims_unit(df = tmp, epoch = '5 sec', dynamic_range = c(-DR, DR), output_mims_per_axis = TRUE)
           x1 = Sys.time()
-          
+
           #=======
           # implement simplified and faster mims:
           MIMSlight_fun = function(df, epochsize=5, acc_columns = 2:4, range=8) {
@@ -243,30 +238,56 @@ if (!file.exists(outputfile) | overwrite == TRUE) {
 DATA = do.call("rbind", combineddata)
 
 # stratify Actigraph by version:
-
-
 MOS = grep(pattern = "MOS", x = DATA$sn)
 CLE = grep(pattern = "CLE", x = DATA$sn)
 DATA$brand[MOS] = "ActigraphMOS"
 DATA$brand[CLE] = "ActigraphCLE"
 
-
 # aggregate
-D2 = aggregate(x = DATA[,c("MIMS_UNIT", "EN", "MAD", "AI", "ENMO", "MIMSlight")],by = list(DATA$HEADER_TIME_STAMP, DATA$brand, DATA$ses_name), FUN = mean)
-myq1 = function(x) as.numeric(quantile(x = x, probs=0.25, na.rm= TRUE))
-myq3 = function(x) as.numeric(quantile(x, probs=0.75, na.rm= TRUE))
-D1 = aggregate(x = DATA[,c("MIMS_UNIT", "EN", "MAD", "AI", "ENMO", "MIMSlight")],by = list(DATA$HEADER_TIME_STAMP, DATA$brand, DATA$ses_name), FUN = myq1)
-D3 = aggregate(x = DATA[,c("MIMS_UNIT", "EN", "MAD", "AI", "ENMO", "MIMSlight")],by = list(DATA$HEADER_TIME_STAMP, DATA$brand, DATA$ses_name), FUN = myq3)
+D2 = aggregate(x = DATA[,c("MIMS_UNIT", "EN", "MAD", "AI", "ENMO", "MIMSlight", "shakefreq")],
+               by = list(DATA$HEADER_TIME_STAMP, DATA$brand, DATA$ses_name, DATA$sn), FUN = mean)
 tidyupname = function(x) {
-  # x = x[,-which(colnames(x) == "brand")]
-  colnames(x)[1:8] = c("time", "brand", "ses_name", "MIMS_UNIT", "EN", "MAD", "AI", "ENMO", "MIMSlight")
+  colnames(x)[1:11] = c("time", "brand", "ses_name", "sn", "MIMS_UNIT",
+                       "EN", "MAD", "AI", "ENMO", "MIMSlight", "shakefreq")
   return(x)
 }
-D1 = tidyupname(D1)
-D2 = tidyupname(D2)
-D3 = tidyupname(D3)
-D = merge(D1, D3, by = c("time", "brand", "ses_name"), suffixes = c("q1","q3"))
-D = merge(D, D2, by = c("time", "brand", "ses_name"))
+D = tidyupname(D2)
+
+
+Dnew = c()
+# remove rare frequencies (bordering with transition period)
+for (ses_name in c("pro2_ses1", "pro2_ses2", "pro2_ses3")) { #"pro3_ses3" ,
+  Dtmp = D[which(D$ses_name == ses_name),]
+  FT = table(Dtmp$shakefreq)
+  conditions_to_exclude = names(FT[which(as.numeric(FT)< 12)])
+  if (length(conditions_to_exclude) > 0) {
+    Dtmp = Dtmp[-which(Dtmp$shakefreq %in% conditions_to_exclude == TRUE),]
+  }
+  if (length(Dnew) == 0) {
+    Dnew = Dtmp
+  } else {
+    Dnew = rbind(Dnew, Dtmp)
+  }
+}
+
+D = Dnew
+# aggregate per shaker frequency
+D$time=as.numeric(D$time)
+D = aggregate(D, list(D$ses_name, D$shakefreq, D$brand, D$sn), FUN = mean)
+colnames(D)[1:4] = c("ses_name", "shakefreq", "brand", "sn")
+D = D[,c("brand","ses_name", "sn", "time", "MIMS_UNIT", "EN", "MAD",
+         "AI", "ENMO", "MIMSlight", "shakefreq",  "brand")]
+
+D$time=as.POSIXlt(D$time, tz = "Europe/Amsterdam", origin ="1970-01-01")
+
+D = D[-which(D$brand == "Activpal"),]
+
+# repeated measures ANOVA
+fitEN = summary(aov(EN ~ brand + Error(shakefreq), data=D[which(D$ses_name=="pro2_ses2"),]))
+fitENMO = summary(aov(ENMO ~ brand + Error(shakefreq), data=D[which(D$ses_name=="pro2_ses2"),]))
+fitMIMSunit = summary(aov(MIMS_UNIT ~ brand + Error(shakefreq), data=D[which(D$ses_name=="pro2_ses2"),]))
+fitMIMSlight = summary(aov(MIMSlight ~ brand + Error(shakefreq), data=D[which(D$ses_name=="pro2_ses2"),]))
+fitAI = summary(aov(AI ~ brand + Error(shakefreq), data=D[which(D$ses_name=="pro2_ses2"),]))
 
 # create plot per session
 pdf(file = "~/data/VUMC/shaker_experiments/inspect_metrics.pdf")
@@ -280,16 +301,15 @@ for (metric in c("MIMS_UNIT", "EN", "MAD", "AI", "ENMO", "MIMSlight")) { #
     AX = which(D$brand == "Axivity" & D$ses_name == ses_name)
     AGC = which(D$brand == "ActigraphCLE" & D$ses_name == ses_name)
     AGM = which(D$brand == "ActigraphMOS" & D$ses_name == ses_name)
-    AP = which(D$brand == "Activpal" & D$ses_name == ses_name)
-    plot(D$time[GA], D[GA, metric], type="l", lwd=1.5, main= ses_name, ylab=metric, 
+    # AP = which(D$brand == "Activpal" & D$ses_name == ses_name)
+    plot(D$time[GA], D[GA, metric], type="p",pch=20, lwd=1.5, main= ses_name, ylab=metric, 
          xlab="time", ylim=YLIM)
-    lines(D$time[AX], D[AX, metric], type="l", col="blue", lwd=1.5)
-    lines(D$time[AGC], D[AGC, metric], type="l", col="red", lwd=1.5)
-    lines(D$time[AGM], D[AGM, metric], type="l", col="green", lwd=1.5)
-    lines(D$time[AP], D[AP, metric], type="l", col="purple", lwd=1.5)
-    legend("topleft",legend = c("GENEActiv","Axivity","Actigraph_CLE","Actigraph_MOS", "Activpal"),
-           col = c("black","blue","red", "green", "purple"), lty=1)
+    lines(D$time[AX], D[AX, metric], type="p",pch=20, col="blue", lwd=1.5)
+    lines(D$time[AGC], D[AGC, metric], type="p",pch=20, col="red", lwd=1.5)
+    lines(D$time[AGM], D[AGM, metric], type="p",pch=20, col="green", lwd=1.5)
+    # lines(D$time[AP], D[AP, metric], type="l", col="purple", lwd=1.5)
+    legend("topleft",legend = c("GENEActiv","Axivity","Actigraph_CLE","Actigraph_MOS"), #, "Activpal"),
+           col = c("black","blue","red", "green"), lty=1) #, "purple"
   }
 }
-
 dev.off()
