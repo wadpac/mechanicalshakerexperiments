03250afa53f25169affed544bc07fa14f9ec8ab0
Vincent van Hees <v.vanhees@accelting.com>
2022-05-27 14:39:11 +0200

    minor edits

--- inst/researchcode/main.R
+++ inst/researchcode/main.R
@@ -12,6 +12,7 @@ for (function_file in dir(my_functions_folder, full.names = T)) source(function_
 #----------------------------------------------------------------
 outputdir = paste0(shaker_experiments_folder, "/structured_raw_data")
 rawdatadir = paste0(shaker_experiments_folder, "/unstructured_raw_data")
+# install.packages(pkgs = c("gdata", "doParallel", "remotes", "GENEAread", "THLfi/read.gt3x", "data.table"))
 
 library(remotes)
 remotes::install_github("wadpac/GGIR")
@@ -23,6 +24,8 @@ lapply(packages, FUN = function(X) {
   X = tmp[length(tmp)]
   do.call("require", list(X)) 
 })
+
+
 #===================================================================================================
 
 brands = c("Actigraph", "MOX", "GENEActiv", "Axivity", "Activpal", "Acttrust") # "Shimmer")
