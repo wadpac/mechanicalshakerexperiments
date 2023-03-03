b717e81a5366633d8296b46db1a5d9ca4b1b6205
Annelinde Lettink <annelinde.lettink@gmail.com>
2023-03-03 16:07:50 +0100

    commit to clean branch

--- inst/researchcode/noise_analyses.R
+++ inst/researchcode/noise_analyses.R
@@ -1,3 +1,8 @@
+#---------------------------------------------------------------------
+# This script assesses the magnitude of noise of the raw acceleration signals in rest across brands and various configurations: dynamic range and sampling frequency.
+# - No-movement segments collected as part of the mechanical shaker experiments (except ms_bag experiment) were used as input
+# - Measure of noise: standard deviation of the raw acceleration signal (x-, y-, and z-axis)
+# - Aggregated per unique combination of brand, sampling frequency, and dynamic range as the median, mean and 95th percentile
 rm(list=ls())
 graphics.off()
 
@@ -6,9 +11,9 @@ shaker_experiments_folder = "/Users/annelindelettink/Documents/Work MacBook Pro 
 # shaker_experiments_folder = "~/data/VUMC/shaker_experiments"
 
 # Specify file paths
-datafile = paste0(shaker_experiments_folder, "/analyses/no_movement.RData")
-noise_output = extracted_data_path = paste0(shaker_experiments_folder, "/analyses/noise")
+datafile = paste0(shaker_experiments_folder, "/analyses/no_movement.RData") # Can be retrieved by running subset_data.R, setting: analysis <- "noise"
 
+noise_output = extracted_data_path = paste0(shaker_experiments_folder, "/analyses/noise")
 if (!dir.exists(noise_output)) {
   dir.create(noise_output)
 }
@@ -18,20 +23,18 @@ if (!dir.exists(noise_output)) {
 load(datafile)
 
 # Exclude two devices (aP_490 and aP_254) from the low sampling frequency experiment (ms_lfcr)
-deviates <- c("aP_490", "aP_254")
-index_deviates <- which(names(data$data) %in% deviates & data$specifications$experiment == "ms_lfcr")
+#deviates <- c("aP_490", "aP_254")
+#index_deviates <- which(names(data$data) %in% deviates & data$specifications$experiment == "ms_lfcr")
+#specifications <- data$specifications[-index_deviates,]
+#data <- data$data
+#data[index_deviates] <- NULL
+#data <- list(data = data, specifications = specifications)
+#rm(specifications, deviates, index_deviates)
 
-specifications <- data$specifications[-index_deviates,]
-data <- data$data
-data[index_deviates] <- NULL
-data <- list(data = data, specifications = specifications)
-rm(specifications, deviates, index_deviates)
-
-# Calculate the standard deviation for all three axes per device
+# Calculate the standard deviation for all three axes for each device
 sd_x <- c()
 sd_y <- c()
 sd_z <- c()
-
 for (device in 1:length(data$data)) {
   df <- data$data[[device]]
   sd_x <- c(sd_x, sd(df$x))
@@ -40,11 +43,33 @@ for (device in 1:length(data$data)) {
 }
 
 noise_data <- cbind(data$specifications, sd_x, sd_y, sd_z)
-noise_data$brand <- as.factor(noise_data$brand)
-noise_data$experiment <- as.factor(noise_data$experiment)
-
+#noise_data$brand <- as.factor(noise_data$brand)
+#noise_data$experiment <- as.factor(noise_data$experiment)
+noise_data$sampling_frequency <- round(as.integer(noise_data$sampling_frequency))
 rm(device, df, sd_x, sd_y, sd_z)
 save(noise_data, file = paste0(noise_output, "/noise.RData"))
+
+#Create descriptive table by aggregating per brand
+# Aggregate per unique combination of brand, dynamic range and sampling frequency
+combinations <- unique(noise_data[c("brand", "dynamic_range", "sampling_frequency")])
+results_agg <- data.frame()
+for(combination in 1:nrow(combinations)){
+  index <- which(noise_data$brand == combinations$brand[combination] & 
+                   noise_data$dynamic_range == combinations$dynamic_range[combination] &
+                   noise_data$sampling_frequency == combinations$sampling_frequency[combination])
+  tmp <- noise_data[index,]
+  if(length(tmp) != 0) {
+    results <- c(brand = unique(tmp$brand), dynamic_range = unique(tmp$dynamic_range), sampling_frequency = unique(tmp$sampling_frequency),
+                     mean_x = mean(tmp$sd_x), mean_y = mean(tmp$sd_y), mean_z = mean(tmp$sd_z), 
+                     median_x = median(tmp$sd_x), median_y = median(tmp$sd_y), median_z = median(tmp$sd_z), 
+                     percentile_x = quantile(tmp$sd_x, .95), percentile_y = quantile(tmp$sd_y, .95), percentile_z = quantile(tmp$sd_z, .95))
+    results_agg <- rbind(results_agg, results)
+  }
+}
+colnames(results_agg) <- c("brand", "dynamic_range", "sampling_frequency", "mean_x", "mean_y", "mean_z",
+                           "median_x", "median_y", "median_z", "percentile95_x", "percentile95_y", "percentile95_z")
+save(results_agg, file = paste0(noise_output, "/noise_aggregated_results.RData"))
+
 
 # Plot noise for the brands with separate panels for the axes
 positions <- c("Actigraph", "Activpal", "Axivity", "GENEActiv", "MOX")
@@ -77,7 +102,19 @@ car::Anova(model.x, type="II", test.statistic="F")
 require(lmerTest)
 test.x <- as(model.x, "merModLmerTest")
 print(summary(test.x, ddf="Kenward-Roger"), correlation = FALSE)
-# Differences in noise between experiments and brand are not dependent on each other
+# There are no differences in noise between experiments within brand
+emmRes.x <- emmeans::emmeans(model.y, comparison="pairwise", 
+                             specs= ~ brand*experiment, adjust="sidak", data = noise_data) # Compute estimated marginal means for the desired fixed effects
+contrast.x <- emmeans::contrast(emmRes.x, "pairwise") # Show pairwise contrasts for the fixed effects
+contrast_df.x <- as.data.frame(contrast.x)
+
+focusOfInterest <- c()
+check <- strsplit(contrast_df.x$contrast, " ")
+for (row in 1:nrow(contrast_df.x)) {
+  focusOfInterest <- c(focusOfInterest, (check[[row]][2] == check[[row]][5] 
+                                         & check[[row]][1] !=   check[[row]][4])) #focus on rows where brand differs, but the experiment is the same
+}
+ct.interest.x <- contrast_df.x[focusOfInterest,]
 
 # y-axis
 model.y <- lme4::lmer(sd_y ~ experiment * brand + (1 | label), data = noise_data)
@@ -98,7 +135,7 @@ for (row in 1:nrow(contrast_df.y)) {
   focusOfInterest <- c(focusOfInterest, (check[[row]][2] == check[[row]][5] 
                                          & check[[row]][1] !=   check[[row]][4])) #focus on rows where brand is the same, but the experiment differs.
 }
-ct.interest.y <- contrast_df.y[focusOfInterest,]
+ct.interest.y <- contrast_df.y[s]
 # There were no differences between experiments for the same brand
 
 # z-axis
